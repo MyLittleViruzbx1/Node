cd320b4c95be61a689bd42e1e702229b6d104643
MyLittleViruzbx1 <MyLittleViruz@outlook.es>
2021-11-28 17:53:04 -0600

    Callback-hell

--- 02-Fundamentos/callback-hell.js
+++ 02-Fundamentos/callback-hell.js
@@ -10,13 +10,13 @@ const empleados = [
     },
     {
         id: 3,
-        nombre: 'Penelope'
+        nombre: 'Mary'
     }
 ];
 
 
 
-const saladios = [
+const salarios = [
     {
         id: 1,
         salario:1000
@@ -25,15 +25,17 @@ const saladios = [
         id: 2,
         salario:2100
     },
-    {
-        id: 3,
-        salario:900
-    }
+    // {
+    //     id: 3,
+    //     salario:900
+    // }
 ];
 
 
 const getEmpleado = (id, callback) => {
-    const empleado = empleados.find( e => e.id === id);
+
+    const empleado = empleados.find( (e) => e.id === id)?.nombre;
+
     if(empleado) {
         callback(null,empleado);
     }else{
@@ -41,14 +43,42 @@ const getEmpleado = (id, callback) => {
     }
 }
 
+
+//Tareas de salario
+const getSalario = (id, callback) => {
+    const salario = salarios.find( (s) => s.id === id)?.salario;//en vez de e era cualquier letra s 
+    //puse pero se puede cualquiera es por buenas practicas que sea diferente
+
+    if(salario) {
+        callback(null,salario);
+    }else{
+        callback(`Salario con id ${id} no existe.`);
+    }
+}
+const id =3;
+
 getEmpleado(1,(err,empleado) =>{
 
     if(err){
         console.log('Error');
         return console.error(err);
     }
+    // console.log('Empleado Encontrado');
+    // console.log(empleado.nombre);
 
-    console.log('Empleado Encontrado');
-    console.log(empleado);
+    getSalario (id, (err, salario)=>{
+        if(err){
+            return console.log(err);
+        }
+        console.log('El empleado: ', empleado, 'Tiene un salario de: ', salario);
+    })
+    
 })
 
+//xxxxxxxxxxxxxcxczcxzczxcz
+// getSalario (id, (err, salario)=>{
+//     if(err){
+//         return console.log(err);
+//     }
+//     console.log(salario);
+// })
