f0972a5757e50452d2e5efea8eea9bec940c9c97
Myz <mylittleviruz@outlook.es>
2023-11-01 06:19:56 -0600

    pensando como hare el ejercicio que me dejaron

--- 03-bases-node/app2.js
+++ 03-bases-node/app2.js
@@ -1,8 +1,8 @@
-
 const { crearArchivo } = require('./helpers/multiplicar');
 
 console.clear();
 
-
 const base = 11;
-crearArchivo(base);
+crearArchivo(base)
+    .then(nombreArchivo => console.log(nombreArchivo, 'creado') )
+    .catch(err =>console.log(err) );
