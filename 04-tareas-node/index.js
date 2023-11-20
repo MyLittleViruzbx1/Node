9259c351ea44e9b42f44d2406f45c9484b7b2a42
Myz <mylittleviruz@outlook.es>
2023-11-20 04:10:51 -0600

    hacer el menu completado

--- 04-tareas-node/index.js
+++ 04-tareas-node/index.js
@@ -1,7 +1,17 @@
 require('colors');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
 console.clear();
+
 const main =async() =>{
     console.log('first')
+    do {
+        
+        opt = await mostrarMenu();
+        console.log({opt})
+       if(opt !== '0') await pausa();
+    } while (opt !== '0');
+    // pausa();
 }
 
 main();
