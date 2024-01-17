96ecdcf2bf2c68cf240d701b471f4980ff797bff
Myz <mylittleviruz@outlook.es>
2024-01-16 23:35:27 -0600

    un pequenio avance

--- 04-tareas-node/helpers/mensajes.js
+++ 04-tareas-node/helpers/mensajes.js
@@ -2,9 +2,16 @@ require('color');
 
 const mostrarMenu = () =>{
     console.clear();
-    console.log('========================');
-    console.log('Selecccione una opcion');
-    console.log('========================')
+    console.log('========================'.green);
+    console.log('Selecccione una opcion'.green);
+    console.log('========================\n'.green)
+    console.log(`${1.} Crear una tarea`);
+    console.log(`${2.} Listar tareas`);
+    console.log(`${3.} Listar tareas copletadas`);
+    console.log(`${4.} Listar tareas pendientes`);
+    console.log(`${5.}Completar tarea(s)`)
+    console.log(`${6.} Borrar tarea`)
+    console.log(`${0.} Salir\n`)
 }
 
 module.exports = {
