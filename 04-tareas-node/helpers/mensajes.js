3aba49dc49af74cddaac9e98cd814dfb7ca6d9e0
Myz <mylittleviruz@outlook.es>
2024-01-18 00:00:50 -0600

    : (

--- 04-tareas-node/helpers/mensajes.js
+++ 04-tareas-node/helpers/mensajes.js
@@ -9,9 +9,9 @@ const mostrarMenu = () =>{
     console.log(`${2.} Listar tareas`);
     console.log(`${3.} Listar tareas copletadas`);
     console.log(`${4.} Listar tareas pendientes`);
-    console.log(`${5.}Completar tarea(s)`)
-    console.log(`${6.} Borrar tarea`)
-    console.log(`${0.} Salir\n`)
+    console.log(`${5.}Completar tarea(s)`);
+    console.log(`${6.} Borrar tarea`);
+    console.log(`${0.} Salir\n`);
 }
 
 module.exports = {
